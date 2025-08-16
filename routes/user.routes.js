c1b462054cc2262739a5f6e06cb91e2f8be95169
me-mayank <174928744+me-mayank@users.noreply.github.com>
2025-08-16 19:11:35 +0530

    updated the user.routes.js with the express app

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,32 +1,27 @@
 import express from "express";
-import {User} from "../models/user.model.js";
+import { User } from "../models/user.model.js";
 
 const router = express.Router();
 
-const app = express();
 
-app.use(express.json());
-
-//create new user
-router.post("/users", async(req,res) => {
-    try{
-        const newUser = await User.create(req.body.json);//inserted data to DataBase
-        res.status(201).json(newUser);
-    }
-    catch(error){
-        res.status(400).json({error: error.message});
-    }
+// Create new user
+router.post("/users", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body); 
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-//get all users 
-router.get("/users", async(req,res) => {
-    try{
-    const user = await User.find();// fetching data from DB
-    res.json(user);
-    }
-    catch(error){
-        res.status(500).json({error: error.message});
-    }
+// Get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 export default router;
