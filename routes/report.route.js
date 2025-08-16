ec0642b83ef104e40c0c69b14a5759015d800da9
me-mayank <174928744+me-mayank@users.noreply.github.com>
2025-08-16 15:09:27 +0530

    added the route to the reports

---
+++ routes/report.route.js
@@ -0,0 +1,29 @@
+import express from "express";
+import {Report} from "../models/report.model.js";
+
+const router = express.Router();
+
+router.post("/report", async(req,res) => {
+     try{
+        const newReport = await Report.create(req.body);//inserted report data in database
+        res.status(201).json(newReport);
+     }
+     catch(error){
+        res.status(400).json({
+            error: error.message
+        });
+     }
+});
+
+// get all reports 
+router.get("/reports", async(req,res) => {
+    try{
+        const report = Report.find();
+        res.json(report);
+    }
+    catch(error){
+        res.json({
+            error: error.message
+        });
+    }
+});
