50c0ee3f19c24665750458e3dac02b9f587255bc
me-mayank <174928744+me-mayank@users.noreply.github.com>
2025-08-19 15:58:05 +0530

    added a good response for GET request to help frontend team

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -28,14 +28,21 @@ router.post("/users", async (req, res) => {
   }
 });
 
-// // Get all users
-// router.get("/users", async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     res.json(users);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
+// Get all users
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json({
+      name: users.name,
+      email: users.email,
+      role: users.role
+    });
+  } catch (error) {
+    res.status(500).json({ 
+      message: "Failed to Fetch Data of all users",
+      error: error.message 
+    });
+  }
+});
 
 export default router;
